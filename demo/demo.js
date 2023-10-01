a5c4633c6d3fe9d4d8427e3212e712197fb42671
Lindsay-Needs-Sleep <lindsay-needs-sleep@outlook.com>
2023-09-30 19:13:04 -0600

    WIP demo - add html embed plugin since we kind of want to mimic it

--- demo/demo.js
+++ demo/demo.js
@@ -3,6 +3,7 @@ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
 
@@ -19,12 +20,13 @@ ClassicEditor
             Heading,
             List,
             Bold,
+            HtmlEmbed,
             SourceEditing,
             FileUploadAndCustomEmbed,
         ],
         toolbar: [
             'heading', 'bold', 'numberedList', 'bulletedList',
-            '|', 'sourceEditing', '|',
+            '|', 'htmlEmbed', 'sourceEditing', '|',
             'addFile',
         ],
         basicFileUploadAndCustomEmbed: basicFileUploadAndCustomEmbedConfig,
